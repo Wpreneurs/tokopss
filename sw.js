2e490a2c73bd24d71c8736381432880e253ee4a0
Wpreneurs <114202814+Wpreneurs@users.noreply.github.com>
2025-10-28 07:43:12 +0700

    Refactor service worker install and fetch events

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ self.addEventListener("install", e => {
         "/?m=1",  // versi mobile
         "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
         "https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap",
-        "https://blogger.googleusercontent.com/img/.../LOGO-SIMPELTOKO-Square-300x300.jpg"
+        "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiZtVhgWU13tcGw_6TEZz84sKawnOBSlTG2-67RIvOccEWpgmnRD9xbBFjICmdwwFtQAzC8ZHDmVa3rJFJa0g7tqOAdIG0r_7_RO8mluwC3R1hpeZgOHQTqAclqilJIZAeWGy7l_SuLxMnW88Qnv1TB7ziV849hp7vZLTITZVCLWMTDG5GUPv4NKDHOaUM/s300/WhatsApp%20Image%202025-10-27%20at%2018.05.06%20(1).jpeg"
       ]);
     })
   );
@@ -19,4 +19,5 @@ self.addEventListener("fetch", e => {
     })
   );
 });
+
 
